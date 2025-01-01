11195b2ee810e5398b74b0306262d9855a077f0a
Isaías José <isaias.jose@xdsoftware.ao>
2025-01-01 23:55:39 +0000

    Add method age in class

--- january/week_1/main.js
+++ january/week_1/main.js
@@ -5,10 +5,17 @@ class Car {
         this.model = model;
         this.year = year;
     }
+
+    age () {
+        const currentYear = new Date();
+        return currentYear.getFullYear() - this.year;
+    }
 }
 
 //Using class Car
 myCar1 = new Car("Mercedes", "VX", 2022);
 myCar2 = new Car("Testa", "XpaceOne", 2024);
 
-document.getElementById("into-class").innerHTML = myCar1.make + " " + myCar2.make;
+//document.getElementById("into-class").innerHTML = myCar1.make + " " + myCar2.make;
+
+document.getElementById("into-class").innerHTML = "My Car 1 is " + myCar1.age() + " years old";
