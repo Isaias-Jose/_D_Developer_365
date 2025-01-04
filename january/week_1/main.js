246b5faf9fb93ea6be992c4169d2511838af7e09
Isaías José <isaias.jose@xdsoftware.ao>
2025-01-04 02:31:15 +0000

    add class-static-method

--- january/week_1/main.js
+++ january/week_1/main.js
@@ -66,5 +66,31 @@ class ElectricCar extends Car {
 const myElectricCar1 = new ElectricCar("Tesla", "Model S", 2022, 100);
 const myElectricCar2 = new ElectricCar("Nissan", "Leaf", 2024, 80);
 
-document.getElementById("title-class-inheritance").innerHTML += "<br>My Electric Car 1 has " + myElectricCar1.getBatteryPercentage() + "% Battery";
-document.getElementById("title-class-inheritance").innerHTML += "<br>My Electric Car 2 has " + myElectricCar2.getBatteryPercentage() + "% Battery";
+document.getElementById("class-inheritance").innerHTML += "<br>My Electric Car 1 has " + myElectricCar1.getBatteryPercentage() + "% Battery";
+document.getElementById("class-inheritance").innerHTML += "<br>My Electric Car 2 has " + myElectricCar2.getBatteryPercentage() + "% Battery";
+
+//----------------------------------------------#DAY 03 - STATIC METHODS-------------------------------------------
+
+//Create static method in Car class
+
+class Certifications {
+    constructor (name, type, time) {
+        this.name = name;
+        this.type = type;
+        this.time = time;
+    }
+    static getCertification(name) {
+        return Certifications.allCertifications.find(certification => certification.name === name);
+    }
+}
+
+Certifications.allCertifications = [
+    new Certifications("B2B", "Motorcycle", 10),
+    new Certifications("B3", "Motorcycle", 15),
+    new Certifications("B3B", "Motorcycle", 20)
+];
+
+//Using static method
+
+const certification1 = Certifications.getCertification("B3B");
+document.getElementById("class-static-method").innerHTML += "<br>B3B certification is " + certification1.time + " years old";
